068f2daece08ba182c703f7102a1d7aa1cae9714
guneyeroglu <benguneyeroglu@gmail.com>
2024-05-20 00:04:24 +0300

    create: new nextjs portfolio side-project starting

---
+++ app/(route)/page.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const HomePage: FC = () => {
+  return (
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <span>Güney Eroğlu Portfolio Website</span>
+    </main>
+  );
+};
+
+export default HomePage;
