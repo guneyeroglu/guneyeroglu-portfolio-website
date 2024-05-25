3c0d7819f151fa4293d73c3dd2592f44f49a212a
guneyeroglu <benguneyeroglu@gmail.com>
2024-05-25 05:13:45 +0300

    #update: structure

--- app/(route)/page.tsx
+++ app/(route)/page.tsx
@@ -1,10 +1,20 @@
 import { FC } from 'react';
 
 const HomePage: FC = () => {
+  const color: string = 'purple';
+
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <span>Güney Eroğlu Portfolio Website</span>
-    </main>
+    <div className='h-full flex flex-col justify-center items-center'>
+      <div className='blur-3xl bg-purple-700/40 w-[450px] h-[450px] rounded fixed left-20 bottom-20 -z-10' />
+      <div className='blur-3xl bg-purple-700/40 w-[450px] h-[450px] rounded fixed right-32 top-32 -z-10' />
+      <span className='text-7xl text-neutral-200'>
+        Hi, I&rsquo;m <span className={`text-${color}-700 text-purple-700`}>Güney</span>.
+      </span>
+      <span className='text-7xl text-neutral-200'>I&rsquo;m a Software Developer.</span>
+      <p className='text-xl text-neutral-500 mt-4'>
+        I develop Front-End and Back-End of Web and Mobile applications.
+      </p>
+    </div>
   );
 };
 
