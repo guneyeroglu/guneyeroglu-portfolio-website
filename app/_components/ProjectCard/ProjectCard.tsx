0b2a1b38c64bd0cfae22adb43263f5a9e8304ba7
guneyeroglu <benguneyeroglu@gmail.com>
2024-06-19 16:35:01 +0300

    #update: hover effect

--- app/_components/ProjectCard/ProjectCard.tsx
+++ app/_components/ProjectCard/ProjectCard.tsx
@@ -27,15 +27,16 @@ const ProjectCard: FC<IProps> = ({ id, label, logo }) => {
       <button className='relative w-[500px] h-72 cursor-pointer text-neutral-200 transition-all hover:text-warning-700 hover:scale-105'>
         <div
           key={id}
-          className='w-full h-full opacity-85 ml-8 rounded-lg brightness-50'
+          className='w-full h-full opacity-85 ml-8 rounded-lg transition-all brightness-50 hover:brightness-100'
           style={{
             backgroundImage: `url(${logo.src})`,
             backgroundPosition: 'top left',
             backgroundSize: 'cover',
             backgroundRepeat: 'no-repeat',
+            maskImage: 'linear-gradient(40deg, transparent 0%, #e5e5e5 40%)',
           }}
         />
-        <div className='absolute bottom-4 left-0 max-w-28 text-left py-4 flex flex-col'>
+        <div className='absolute bottom-2 left-0 max-w-28 text-left py-4 flex flex-col'>
           <span className='text-base text-current font-medium'>{label}</span>
           <span className='text- base text-current font-light border-t-1.5 border-t-current mt-2 pt-2'>
             {addIdZero()}
